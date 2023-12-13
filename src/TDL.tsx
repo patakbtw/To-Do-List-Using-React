cf5b5061cbc2a946ea458eb3a1983711cf2c420f
Мелешкин Виктор <9580573@gmail.com>
2023-12-13 21:10:20 +0000

    wip

--- src/TDL.tsx
+++ src/TDL.tsx
@@ -1,10 +1,15 @@
 import "./styles/TDL.scss";
 import React, { useState } from "react";
+import trash from '../public/trash.svg'
 
 type FormProps = {
   // You also can use type like this React.Dispatch<React.SetStateAction<string[]>>
   onHandleSubmit: (array: (state: string[]) => string[]) => void;
 };
+
+type ButtonProps = {
+  funNumber: number;
+}
 
 const TDL_form = (props: FormProps) => {
   const [textInput, setTextInput] = useState<string>("");
@@ -44,18 +49,33 @@ const TDL_form = (props: FormProps) => {
   );
 };
 
+const TDL_delete = (props: FormProps, coolProps: ButtonProps) => {
+  const {onHandleSubmit} = props;
+  const {funNumber} = coolProps;
+
+  const handleDelete = (index : number) => {
+    onHandleSubmit((prevTaskList: string[]) =>  prevTaskList.splice(index, 1));
+  }
+
+  return(
+    <button className="tdl__dlt" onClick={()=>{handleDelete(funNumber)}}>
+      <img src={trash} alt="" />
+    </button>
+  );
+}
+
 const TDL_li = (task: string, index: number): JSX.Element => {
   // Line 50. key should be stable across renders https://react.dev/learn/rendering-lists#rules-of-keys
   const newId = Math.random().toString(36);
+
+
   return (
     <li className="tdl__item" key={task}>
       <input type="checkbox" className="tdl__chkbox" id={`${newId}_${index}`} />
       <label htmlFor={`${newId}_${index}`} className="tdl__task">
         {task}
       </label>
-      <button className="tdl__dlt">
-        <img alt="" />
-      </button>
+    <TDL_delete onHandleSubmit={}/>
     </li>
   );
 };
